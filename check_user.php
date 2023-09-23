99906a356b35a6755282a75802c5ff82526d000f
Luis Munoz <lmunoz@cryptorepair.com>
2023-09-23 19:32:09 -0400

    feat: cehck user exist on db

---
+++ check_user.php
@@ -0,0 +1,54 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+//header('Content-Type: application/json;charset=utf-8');
+header('Access-Control-Allow-Methods:  GET');
+require_once 'vendor/autoload.php';
+
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Database connection parameters
+$dbHost = $_ENV['DB_HOST'];
+$dbPort = $_ENV['DB_PORT'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+
+$method = $_SERVER['REQUEST_METHOD'];
+// Check if the form has been submitted
+if ($method == "GET") {
+    try {
+        // Create a PDO instance for the database connection
+        $pdo = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        // Get the form data 
+        $raw = file_get_contents('php://input');
+        $data = json_decode($raw, true);
+        // Load Privy id
+        $privy_id = $data['privy_id'];
+        
+        // Query to check if the user exists
+        $sql = 'SELECT COUNT(*) FROM quiztopia_users WHERE privy_id = :privy_id';
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(':username',$privy_id);
+        $statement->execute();
+        
+        $count = $statement->fetchColumn();
+        
+        if ($count > 0) {
+            $output = array(
+                'status_code' => 200,
+                'message' => 'User Exist.'
+            );
+        } else {
+            $output = array(
+                'status_code' => 300,
+                'message' => 'User does not exist'
+            );
+        }
+        echo json_encode($output);
+    } catch (PDOException $e) {
+        die("Error: " . $e->getMessage());
+    }
+}
